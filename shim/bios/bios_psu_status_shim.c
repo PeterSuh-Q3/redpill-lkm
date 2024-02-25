038cac2eb4e48ba962fa446d9d1f62c34a384497
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-02-25 15:22:20 +0900

    sync to rr

--- shim/bios/bios_psu_status_shim.c
+++ shim/bios/bios_psu_status_shim.c
@@ -14,27 +14,28 @@
 static const struct hw_config *hw_config = NULL;
 static override_symbol_inst *HWMONGetPSUStatusByI2C_ovs = NULL;
 static override_symbol_inst *RS4021xspI2CGetPowerInfo_ovs = NULL;
-static override_symbol_inst *FS2500I2CGetPowerInfo_ovs = NULL;
-static override_symbol_inst *SA6400I2CGetPowerInfo_ovs = NULL;
+static override_symbol_inst *RS4022xspI2CGetPowerInfo_ovs = NULL;
+static override_symbol_inst *RS4023xspI2CGetPowerInfo_ovs = NULL;
+static override_symbol_inst *RS4024xspI2CGetPowerInfo_ovs = NULL;
+static override_symbol_inst *FS3410I2CGetPowerInfo_ovs = NULL;
+static override_symbol_inst *FS6400I2CGetPowerInfo_ovs = NULL;
+static override_symbol_inst *FS6500I2CGetPowerInfo_ovs = NULL;
+static override_symbol_inst *HD6500I2CGetPowerInfo_ovs = NULL;
+static override_symbol_inst *SA6500I2CGetPowerInfo_ovs = NULL;
 
 static int HWMONGetPSUStatusByI2C_shim(void)
 {
-    return 1;
+    return 0;
 }
 
-static int RS4021xspI2CGetPowerInfo_shim(void)
+static int HWMONI2CGetPowerInfo_shim(void)
 {
-    return 1;
+    return 0;
 }
 
-static int FS2500I2CGetPowerInfo_shim(void)
+int I2CSmbusReadPowerStatus(void)
 {
-    return 1;
-}
-
-static int SA6400I2CGetPowerInfo_shim(void)
-{
-    return 1;
+    return 0;
 }
 
 int register_bios_psu_status_shim(const struct hw_config *hw)
@@ -45,68 +46,134 @@ int register_bios_psu_status_shim(const struct hw_config *hw)
         shim_reg_already();
     if (unlikely(RS4021xspI2CGetPowerInfo_ovs))
         shim_reg_already();
-    if (unlikely(FS2500I2CGetPowerInfo_ovs))
+    if (unlikely(RS4022xspI2CGetPowerInfo_ovs))
         shim_reg_already();
-    if (unlikely(SA6400I2CGetPowerInfo_ovs))
+    if (unlikely(RS4023xspI2CGetPowerInfo_ovs))
         shim_reg_already();
-
+    if (unlikely(RS4024xspI2CGetPowerInfo_ovs))
+        shim_reg_already();
+    if (unlikely(FS3410I2CGetPowerInfo_ovs))
+        shim_reg_already();
+    if (unlikely(FS6400I2CGetPowerInfo_ovs))
+        shim_reg_already();
+    if (unlikely(FS6500I2CGetPowerInfo_ovs))
+        shim_reg_already();
+    if (unlikely(HD6500I2CGetPowerInfo_ovs))
+        shim_reg_already();
+    if (unlikely(SA6500I2CGetPowerInfo_ovs))
+        shim_reg_already();
+        
     hw_config = hw;
     override_symbol_or_exit_int(HWMONGetPSUStatusByI2C_ovs, "HWMONGetPSUStatusByI2C", HWMONGetPSUStatusByI2C_shim);
-    override_symbol_or_exit_int(RS4021xspI2CGetPowerInfo_ovs, "RS4021xspI2CGetPowerInfo_ovs", RS4021xspI2CGetPowerInfo_shim);
-    override_symbol_or_exit_int(FS2500I2CGetPowerInfo_ovs, "FS2500I2CGetPowerInfo_ovs", FS2500I2CGetPowerInfo_shim);    
-    override_symbol_or_exit_int(SA6400I2CGetPowerInfo_ovs, "SA6400I2CGetPowerInfo_ovs", SA6400I2CGetPowerInfo_shim);    
-
+    override_symbol_or_exit_int(RS4021xspI2CGetPowerInfo_ovs, "RS4021xspI2CGetPowerInfo", HWMONI2CGetPowerInfo_shim);
+    override_symbol_or_exit_int(RS4022xspI2CGetPowerInfo_ovs, "RS4022xspI2CGetPowerInfo", HWMONI2CGetPowerInfo_shim);
+    override_symbol_or_exit_int(RS4023xspI2CGetPowerInfo_ovs, "RS4023xspI2CGetPowerInfo", HWMONI2CGetPowerInfo_shim);
+    override_symbol_or_exit_int(RS4024xspI2CGetPowerInfo_ovs, "RS4024xspI2CGetPowerInfo", HWMONI2CGetPowerInfo_shim);
+    override_symbol_or_exit_int(FS3410I2CGetPowerInfo_ovs, "FS3410I2CGetPowerInfo", HWMONI2CGetPowerInfo_shim);
+    override_symbol_or_exit_int(FS6400I2CGetPowerInfo_ovs, "FS6400I2CGetPowerInfo", HWMONI2CGetPowerInfo_shim);
+    override_symbol_or_exit_int(FS6500I2CGetPowerInfo_ovs, "FS6500I2CGetPowerInfo", HWMONI2CGetPowerInfo_shim);
+    override_symbol_or_exit_int(HD6500I2CGetPowerInfo_ovs, "HD6500I2CGetPowerInfo", HWMONI2CGetPowerInfo_shim);
+    override_symbol_or_exit_int(SA6500I2CGetPowerInfo_ovs, "SA6500I2CGetPowerInfo", HWMONI2CGetPowerInfo_shim);
     shim_reg_ok();
     return 0;
 }
 
 int unregister_bios_psu_status_shim(void)
 {
+    int out = 0;
     shim_ureg_in();
 
     if (unlikely(!HWMONGetPSUStatusByI2C_ovs))
         return 0; //this is deliberately a noop
     if (unlikely(!RS4021xspI2CGetPowerInfo_ovs))
         return 0; //this is deliberately a noop
-    if (unlikely(!FS2500I2CGetPowerInfo_ovs))
+    if (unlikely(!RS4022xspI2CGetPowerInfo_ovs))
         return 0; //this is deliberately a noop
-    if (unlikely(!SA6400I2CGetPowerInfo_ovs))
+    if (unlikely(!RS4023xspI2CGetPowerInfo_ovs))
+        return 0; //this is deliberately a noop
+    if (unlikely(!RS4024xspI2CGetPowerInfo_ovs))
+        return 0; //this is deliberately a noop
+    if (unlikely(!FS3410I2CGetPowerInfo_ovs))
+        return 0; //this is deliberately a noop
+    if (unlikely(!FS6400I2CGetPowerInfo_ovs))
+        return 0; //this is deliberately a noop
+    if (unlikely(!FS6500I2CGetPowerInfo_ovs))
+        return 0; //this is deliberately a noop
+    if (unlikely(!HD6500I2CGetPowerInfo_ovs))
+        return 0; //this is deliberately a noop
+    if (unlikely(!SA6500I2CGetPowerInfo_ovs))
         return 0; //this is deliberately a noop
 
-    int out = restore_symbol(HWMONGetPSUStatusByI2C_ovs);
+    out = restore_symbol(HWMONGetPSUStatusByI2C_ovs);
     if (unlikely(out != 0)) {
-        pr_loc_err("Failed to restore HWMONGetPSUStatusByI2C - error=%d", out);
+        pr_loc_err("Failed to restore HWMONGetPSUStatusByI2C_ovs - error=%d", out);
         return out;
     }
     HWMONGetPSUStatusByI2C_ovs = NULL;
 
-    int outrs4021xsp = restore_symbol(RS4021xspI2CGetPowerInfo_ovs);
-    if (unlikely(outrs4021xsp != 0)) {
-        pr_loc_err("Failed to restore RS4021xspI2CGetPowerInfo - error=%d", out);
+    out = restore_symbol(RS4021xspI2CGetPowerInfo_ovs);
+    if (unlikely(out != 0)) {
+        pr_loc_err("Failed to restore RS4021xspI2CGetPowerInfo_ovs - error=%d", out);
         return out;
     }
-    HWMONGetPSUStatusByI2C_ovs = NULL;
     RS4021xspI2CGetPowerInfo_ovs = NULL;
 
-    int outfs2500 = restore_symbol(FS2500I2CGetPowerInfo_ovs);
-    if (unlikely(outfs2500 != 0)) {
-        pr_loc_err("Failed to restore FS2500I2CGetPowerInfo - error=%d", out);
+    out = restore_symbol(RS4022xspI2CGetPowerInfo_ovs);
+    if (unlikely(out != 0)) {
+        pr_loc_err("Failed to restore RS4022xspI2CGetPowerInfo_ovs - error=%d", out);
         return out;
     }
-    HWMONGetPSUStatusByI2C_ovs = NULL;
-    RS4021xspI2CGetPowerInfo_ovs = NULL;
-    FS2500I2CGetPowerInfo_ovs = NULL;
+    RS4022xspI2CGetPowerInfo_ovs = NULL;
 
-    int outsa6400 = restore_symbol(SA6400I2CGetPowerInfo_ovs);
-    if (unlikely(outsa6400 != 0)) {
-        pr_loc_err("Failed to restore SA6400I2CGetPowerInfo - error=%d", out);
+    out = restore_symbol(RS4023xspI2CGetPowerInfo_ovs);
+    if (unlikely(out != 0)) {
+        pr_loc_err("Failed to restore RS4023xspI2CGetPowerInfo_ovs - error=%d", out);
         return out;
     }
-    HWMONGetPSUStatusByI2C_ovs = NULL;
-    RS4021xspI2CGetPowerInfo_ovs = NULL;
-    FS2500I2CGetPowerInfo_ovs = NULL;
-    SA6400I2CGetPowerInfo_ovs = NULL;
-    
+    RS4023xspI2CGetPowerInfo_ovs = NULL;
+
+    out = restore_symbol(RS4024xspI2CGetPowerInfo_ovs);
+    if (unlikely(out != 0)) {
+        pr_loc_err("Failed to restore RS4024xspI2CGetPowerInfo_ovs - error=%d", out);
+        return out;
+    }
+    RS4024xspI2CGetPowerInfo_ovs = NULL;
+
+    out = restore_symbol(FS3410I2CGetPowerInfo_ovs);
+    if (unlikely(out != 0)) {
+        pr_loc_err("Failed to restore FS3410I2CGetPowerInfo_ovs - error=%d", out);
+        return out;
+    }
+    FS3410I2CGetPowerInfo_ovs = NULL;
+
+    out = restore_symbol(FS6400I2CGetPowerInfo_ovs);
+    if (unlikely(out != 0)) {
+        pr_loc_err("Failed to restore FS6400I2CGetPowerInfo_ovs - error=%d", out);
+        return out;
+    }
+    FS6400I2CGetPowerInfo_ovs = NULL;
+
+    out = restore_symbol(FS6500I2CGetPowerInfo_ovs);
+    if (unlikely(out != 0)) {
+        pr_loc_err("Failed to restore FS6500I2CGetPowerInfo_ovs - error=%d", out);
+        return out;
+    }
+    FS6500I2CGetPowerInfo_ovs = NULL;
+
+    out = restore_symbol(HD6500I2CGetPowerInfo_ovs);
+    if (unlikely(out != 0)) {
+        pr_loc_err("Failed to restore HD6500I2CGetPowerInfo_ovs - error=%d", out);
+        return out;
+    }
+    HD6500I2CGetPowerInfo_ovs = NULL;
+
+    out = restore_symbol(SA6500I2CGetPowerInfo_ovs);
+    if (unlikely(out != 0)) {
+        pr_loc_err("Failed to restore SA6500I2CGetPowerInfo_ovs - error=%d", out);
+        return out;
+    }
+    SA6500I2CGetPowerInfo_ovs = NULL;
+
     shim_ureg_ok();
     return 0;
 }
@@ -119,12 +186,22 @@ int reset_bios_psu_status_shim(void)
 
     put_overridden_symbol(RS4021xspI2CGetPowerInfo_ovs);
     RS4021xspI2CGetPowerInfo_ovs = NULL;
-
-    put_overridden_symbol(FS2500I2CGetPowerInfo_ovs);
-    FS2500I2CGetPowerInfo_ovs = NULL;
-
-    put_overridden_symbol(SA6400I2CGetPowerInfo_ovs);
-    SA6400I2CGetPowerInfo_ovs = NULL;
+    put_overridden_symbol(RS4022xspI2CGetPowerInfo_ovs);
+    RS4022xspI2CGetPowerInfo_ovs = NULL;
+    put_overridden_symbol(RS4023xspI2CGetPowerInfo_ovs);
+    RS4023xspI2CGetPowerInfo_ovs = NULL;
+    put_overridden_symbol(RS4024xspI2CGetPowerInfo_ovs);
+    RS4024xspI2CGetPowerInfo_ovs = NULL;
+    put_overridden_symbol(FS3410I2CGetPowerInfo_ovs);
+    FS3410I2CGetPowerInfo_ovs = NULL;
+    put_overridden_symbol(FS6400I2CGetPowerInfo_ovs);
+    FS6400I2CGetPowerInfo_ovs = NULL;
+    put_overridden_symbol(FS6500I2CGetPowerInfo_ovs);
+    FS6500I2CGetPowerInfo_ovs = NULL;
+    put_overridden_symbol(HD6500I2CGetPowerInfo_ovs);
+    HD6500I2CGetPowerInfo_ovs = NULL;
+    put_overridden_symbol(SA6500I2CGetPowerInfo_ovs);
+    SA6500I2CGetPowerInfo_ovs = NULL;
     
     shim_reset_ok();
     return 0;

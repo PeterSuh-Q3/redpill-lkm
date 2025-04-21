d9e816f42f6ef119ca59364ab7a52b79ce4be4d4
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2025-04-21 16:50:45 +0900

    ADD RP_PLATFORM_AVATON

--- config/platforms.h
+++ config/platforms.h
@@ -141,6 +141,25 @@ const struct hw_config platformConfig = {
         .psu_status = {HWMON_PSU_NULL_ID},
         .sys_current = {HWMON_SYS_CURR_NULL_ID},
     }};
+#elif defined(RP_PLATFORM_AVATON)
+const struct hw_config platformConfig = {
+    .name = "", // "DS1515+, DS1815+, DS415+, DS2415+, RS815+, RS815RP+, RS2416+, RS2416RP+",
+    .pci_stubs = {
+        {.type = __VPD_TERMINATOR__}},
+    .emulate_rtc = false,
+    .swap_serial = false,
+    .reinit_ttyS0 = true,
+    .fix_disk_led_ctrl = true,
+    .has_cpu_temp = true,
+    .is_dt = false,
+    .hwmon = {
+        .sys_thermal = {HWMON_SYS_TZONE_REMOTE1_ID, HWMON_SYS_TZONE_LOCAL_ID, HWMON_SYS_TZONE_REMOTE2_ID},
+        .sys_voltage = {HWMON_SYS_VSENS_VCC_ID, HWMON_SYS_VSENS_VPP_ID, HWMON_SYS_VSENS_V33_ID, HWMON_SYS_VSENS_V5_ID, HWMON_SYS_VSENS_V12_ID},
+        .sys_fan_speed_rpm = {HWMON_SYS_FAN1_ID, HWMON_SYS_FAN2_ID},
+        .hdd_backplane = {HWMON_SYS_HDD_BP_NULL_ID},
+        .psu_status = {HWMON_PSU_NULL_ID},
+        .sys_current = {HWMON_SYS_CURR_NULL_ID},
+    }};
 #elif defined(RP_PLATFORM_GEMINILAKE)
 const struct hw_config platformConfig = {
     .name = "", // "DS220+ DS420+ DS720+ DS920+ DS1520+ DVA1622 DS423+ DS224+",
